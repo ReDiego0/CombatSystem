3e12990f151088793b8a8d2ff911d7be2c16c940
ReDiego0 <your.email@example.com>
2026-07-13 14:33:37 -0400

    Initial commit

---
+++ src/main/kotlin/org/ReDiego0/combatSystem/CombatSystem.kt
@@ -0,0 +1,14 @@
+package org.ReDiego0.combatSystem
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class CombatSystem : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
